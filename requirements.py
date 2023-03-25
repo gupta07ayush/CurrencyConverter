b01944639f7d2565b9ea1a1bae3842bfe44a17cc
Ayush Gupta <gupta07ayush@gmail.com>
2023-03-25 16:34:38 +0530

    Create requirements.py

---
+++ requirements.py
@@ -0,0 +1,10 @@
+python 3.x
+pip install tkinter
+
+<h3>Check for Built-in Tkinter Package</h3>
+  
+As mentioned above, the Tkinter package also comes pre-installed with python 3.7 and higher. 
+This essentially means that the user might not need to explicitly install the Tkinter package as it may already be installed on his machine.
+To verify this, run the following command inside the Command Prompt:
+
+python -m tkinter
